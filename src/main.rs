6748d4c649c029b6bb6892b58353e55f31ff8868
yttnn <67893139+yttnn@users.noreply.github.com>
2024-07-13 13:49:00 +0900

    finish 2.4

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 fn main() {
-  let mut stack = vec![];
-  stack.push(42);
-  stack.push(36);
-
-  add(&mut stack);
-
-  stack.push(22);
-
-  add(&mut stack);
-
-  println!("stack: {stack:?}");
-}
-
-fn add(stack: &mut Vec<i32>) {
-  let lhs = stack.pop().unwrap();
-  let rhs = stack.pop().unwrap();
-  stack.push(lhs + rhs);
+  for line in std::io::stdin().lines() {
+    if let Ok(line) = line {
+      let words: Vec<_> = line.split(" ").collect();
+      println!("Line: {words:?}");
+    }
+  }
 }
