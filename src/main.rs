bbeef6925b03df111a39ec01828d02cf78f43760
yttnn <67893139+yttnn@users.noreply.github.com>
2024-07-14 21:40:01 +0900

    2.5 impl

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,102 @@
-fn main() {
-  for line in std::io::stdin().lines() {
-    let mut stack = vec![];
-    if let Ok(line) = line {
-      let words: Vec<_> = line.split(" ").collect();
 
-      for word in words {
-        if let Ok(parsed) = word.parse::<i32>() {
-          stack.push(parsed);
-        } else {
-          match word {
-            "+" => add(&mut stack),
-            "-" => sub(&mut stack),
-            "*" => mul(&mut stack),
-            "/" => div(&mut stack),
-            _ => panic!("{word:?} could not be parsed"),
-          }
-        }
-      }
+#[derive(Debug, PartialEq, Eq)]
+enum Value<'src> {
+  Num(i32),
+  Op(&'src str),
+  Block(Vec<Value<'src>>),
+}
 
-      println!("Line: {stack:?}");
+impl<'src> Value<'src> {
+  fn as_num(&self) -> i32 {
+    match self {
+      Self::Num(val) => *val,
+      _ => panic!("Value is not a number"),
     }
   }
 }
 
-fn add(stack: &mut Vec<i32>) {
-  let lhs = stack.pop().unwrap();
-  let rhs = stack.pop().unwrap();
-  stack.push(lhs + rhs);
+fn main() {
+  for line in std::io::stdin().lines().flatten() {
+    parse(&line);
+  }
 }
 
-fn sub(stack: &mut Vec<i32>) {
-  let rhs = stack.pop().unwrap();
-  let lhs = stack.pop().unwrap();
-  stack.push(lhs - rhs);
+fn parse<'a> (line: &'a str) -> Vec<Value> {
+  let mut stack = vec![];
+  let input: Vec<_> = line.split(" ").collect();
+  let mut words = &input[..];
+
+  while let Some((&word, mut rest)) = words.split_first() {
+    if word.is_empty() {
+      break;
+    }
+    if word == "{" {
+      let value;
+      (value, rest) = parse_block(rest);
+      stack.push(value);
+    } else if let Ok(parsed) = word.parse::<i32>() {
+      stack.push(Value::Num(parsed));
+    } else {
+      match word {
+        "+" => add(&mut stack),
+        "-" => sub(&mut stack),
+        "*" => mul(&mut stack),
+        "/" => div(&mut stack),
+        _ => panic!("{word:?} could not be parsed"),
+      }
+    }
+    words = rest;
+  }
+
+  println!("Line: {stack:?}");
+  stack
 }
 
-fn mul(stack: &mut Vec<i32>) {
-  let rhs = stack.pop().unwrap();
-  let lhs = stack.pop().unwrap();
-  stack.push(lhs * rhs);
+fn parse_block<'src, 'a>(input: &'a [&'src str]) -> (Value<'src>, &'a [&'src str]) {
+  let mut tokens = vec![];
+  let mut words = input;
+
+  while let Some((&word, mut rest)) = words.split_first() {
+    if word.is_empty() {
+      break;
+    }
+    if word == "{" {
+      let value;
+      (value, rest) = parse_block(rest);
+      tokens.push(value);
+    } else if word == "}" {
+      return (Value::Block(tokens), rest);
+    } else if let Ok(value) = word.parse::<i32>() {
+      tokens.push(Value::Num(value));
+    } else {
+      tokens.push(Value::Op(word));
+    }
+    words = rest;
+  }
+
+  (Value::Block(tokens), words)
 }
 
-fn div(stack: &mut Vec<i32>) {
-  let rhs = stack.pop().unwrap();
-  let lhs = stack.pop().unwrap();
-  stack.push(lhs / rhs);
+fn add(stack: &mut Vec<Value>) {
+  let lhs = stack.pop().unwrap().as_num();
+  let rhs = stack.pop().unwrap().as_num();
+  stack.push(Value::Num(lhs + rhs));
+}
+
+fn sub(stack: &mut Vec<Value>) {
+  let rhs = stack.pop().unwrap().as_num();
+  let lhs = stack.pop().unwrap().as_num();
+  stack.push(Value::Num(lhs - rhs));
+}
+
+fn mul(stack: &mut Vec<Value>) {
+  let rhs = stack.pop().unwrap().as_num();
+  let lhs = stack.pop().unwrap().as_num();
+  stack.push(Value::Num(lhs * rhs));
+}
+
+fn div(stack: &mut Vec<Value>) {
+  let rhs = stack.pop().unwrap().as_num();
+  let lhs = stack.pop().unwrap().as_num();
+  stack.push(Value::Num(lhs / rhs));
 }
