b106e326bea69e2b5c484a0abaf16e21cdfb99d9
yttnn <67893139+yttnn@users.noreply.github.com>
2024-07-15 23:22:34 +0900

    finish 2.8

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::{BufRead, BufReader}};
 
-struct Vm<'src> {
-  stack: Vec<Value<'src>>,
-  vars: HashMap<&'src str, Value<'src>>,
+struct Vm {
+  stack: Vec<Value>,
+  vars: HashMap<String, Value>,
+  blocks: Vec<Vec<Value>>,
 }
 
-impl<'src> Vm<'src> {
+impl Vm {
   fn new() -> Self {
     Self {
       stack: vec![],
       vars: HashMap::new(),
+      blocks: vec![],
     }
   }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-enum Value<'src> {
+enum Value {
   Num(i32),
-  Op(&'src str),
-  Sym(&'src str),
-  Block(Vec<Value<'src>>),
+  Op(String),
+  Sym(String),
+  Block(Vec<Value>),
 }
 
-impl<'src> Value<'src> {
+impl Value {
   fn as_num(&self) -> i32 {
     match self {
       Self::Num(val) => *val,
@@ -30,62 +32,87 @@ impl<'src> Value<'src> {
     }
   }
 
-  fn as_sym(&self) -> &'src str {
+  fn as_sym(&self) -> &str {
     if let Self::Sym(sym) = self {
-      *sym
+      sym
     } else {
       panic!("Value is not a symbol");
     }
   }
 
-  fn to_block(self) -> Vec<Value<'src>> {
+  fn to_block(self) -> Vec<Value> {
     match self {
       Self::Block(val) => val,
       _ => panic!("value is not a block"),
     }
   }
+
+  fn to_string(&self) -> String {
+    match self {
+      Self::Num(i) => i.to_string(),
+      Self::Op(ref s) | Self::Sym(ref s) => s.clone(),
+      Self::Block(_) => "<Block>".to_string(),
+    }
+  }
 }
 
 fn main() {
-  for line in std::io::stdin().lines().flatten() {
-    parse(&line);
+  if let Some(f) = std::env::args().nth(1).and_then(|f| std::fs::File::open(f).ok()) {
+    parse_batch(BufReader::new(f));
+  } else {
+    parse_interactive();
   }
 }
 
-fn parse<'a> (line: &'a str) -> Vec<Value> {
+fn parse_batch(source: impl BufRead) -> Vec<Value> {
   let mut vm = Vm::new();
-  let input: Vec<_> = line.split(" ").collect();
-  let mut words = &input[..];
-
-  while let Some((&word, mut rest)) = words.split_first() {
-    if word.is_empty() {
-      break;
+  for line in source.lines().flatten() {
+    for word in line.split(" ") {
+      parse_word(word, &mut vm);  
     }
-    if word == "{" {
-      let value;
-      (value, rest) = parse_block(rest);
-      vm.stack.push(value);
-    } else {
-      let code = if let Ok(num) = word.parse::<i32>() {
-        Value::Num(num)
-      } else if word.starts_with("/") {
-        Value::Sym(&word[1..])
-      } else {
-        Value::Op(word)
-      };
-      eval(code, &mut vm);
-    }
-    words = rest;
-    println!("{:?}", vm.stack);
   }
-
-  println!("Line: {:?}", vm.stack);
   vm.stack
 }
 
-fn eval<'src>(code: Value<'src>, vm: &mut Vm<'src>) {
+fn parse_interactive() {
+  let mut vm = Vm::new();
+  for line in std::io::stdin().lines().flatten() {
+    for word in line.split(" ") {
+      parse_word(word, &mut vm);
+    }
+    println!("stack: {:?}", vm.stack);
+  }
+}
+
+fn parse_word(word: &str, vm: &mut Vm) {
+  if word.is_empty() {
+    return;
+  }
+  if word == "{" {
+    vm.blocks.push(vec![]);
+  } else if word == "}" {
+    let top_block = vm.blocks.pop().expect("Block stack underrun");
+    eval(Value::Block(top_block), vm);
+  } else {
+    let code = if let Ok(num) = word.parse::<i32>() {
+      Value::Num(num)
+    } else if word.starts_with("/") {
+      Value::Sym(word[1..].to_string())
+    } else {
+      Value::Op(word.to_string())
+    };
+    eval(code, vm);
+  }
+  println!("stack:{:?} block:{:?}", vm.stack, vm.blocks);
+}
+
+fn eval(code: Value, vm: &mut Vm) {
+  if let Some(top_block) = vm.blocks.last_mut() {
+    top_block.push(code);
+    return;
+  }
   match code {
-    Value::Op(op) => match op {
+    Value::Op(ref op) => match op as &str {
       "+" => add(&mut vm.stack),
       "-" => sub(&mut vm.stack),
       "*" => mul(&mut vm.stack),
@@ -93,6 +120,7 @@ fn eval<'src>(code: Value<'src>, vm: &mut Vm<'src>) {
       "<" => lt(&mut vm.stack),
       "if" => op_if(vm),
       "def" => op_def(vm),
+      "puts" => puts(vm),
       _ => {
         let val = vm.vars.get(op).expect(&format!("{op:?} is not a defined operation"));
         vm.stack.push(val.clone());
@@ -100,31 +128,6 @@ fn eval<'src>(code: Value<'src>, vm: &mut Vm<'src>) {
     },
     _ => vm.stack.push(code.clone()),
   }
-}
-
-fn parse_block<'src, 'a>(input: &'a [&'src str]) -> (Value<'src>, &'a [&'src str]) {
-  let mut tokens = vec![];
-  let mut words = input;
-
-  while let Some((&word, mut rest)) = words.split_first() {
-    if word.is_empty() {
-      break;
-    }
-    if word == "{" {
-      let value;
-      (value, rest) = parse_block(rest);
-      tokens.push(value);
-    } else if word == "}" {
-      return (Value::Block(tokens), rest);
-    } else if let Ok(value) = word.parse::<i32>() {
-      tokens.push(Value::Num(value));
-    } else {
-      tokens.push(Value::Op(word));
-    }
-    words = rest;
-  }
-
-  (Value::Block(tokens), words)
 }
 
 macro_rules! impl_op {
@@ -169,14 +172,25 @@ fn op_def(vm: &mut Vm) {
   let value = vm.stack.pop().unwrap();
   eval(value, vm);
   let value = vm.stack.pop().unwrap();
-  let sym = vm.stack.pop().unwrap().as_sym();
+  let sym = vm.stack.pop().unwrap().as_sym().to_string();
 
   vm.vars.insert(sym, value);
 }
 
+fn puts(vm: &mut Vm) {
+  let value = vm.stack.pop().unwrap();
+  println!("{}", value.to_string());
+}
+
 #[cfg(test)]
 mod test {
-  use super::{parse, Value::*};
+  use std::io::Cursor;
+  use super::{Value::*, *};
+
+  fn parse(input: &str) -> Vec<Value> {
+    parse_batch(Cursor::new(input))
+  }
+
   #[test]
   fn test_group() {
     assert_eq!(
@@ -213,6 +227,24 @@ mod test {
   fn test_var_if() {
     assert_eq!(
       parse("/x 10 def /y 20 def { x y < } { x } { y } if"),
+      vec![Num(10)]
+    );
+  }
+
+  #[test]
+  fn test_multiline() {
+    assert_eq!(
+      parse(
+        r#"
+        /x 10 def
+        /y 20 def
+
+        { x y < }
+        { x }
+        { y }
+        if
+        "#
+      ),
       vec![Num(10)]
     );
   }
